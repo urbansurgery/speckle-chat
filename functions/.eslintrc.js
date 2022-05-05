f82f428502cfeaf7ed25e5ef1c002dbb9be6aac1
Jonathon Broughton <jonathon@stardotbmp.com>
2022-05-05 16:10:15 +0100

    chore (editor): Syncing editor style/lint settings to the speckle project

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,33 +1,42 @@
 module.exports = {
   root: true,
   env: {
-    es6: true,
+    es2021: true,
     node: true,
+    commonjs: true
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-    tsconfigRootDir: __dirname,
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "**/*.json", // Ignore JSON files.
+    '/lib/**/*', // Ignore built files.
+    '**/*.json' // Ignore JSON files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-  },
-};
+    allowImportExportEverywhere: 0,
+    camelcase: [
+      1,
+      {
+        properties: 'always'
+      }
+    ],
+    'no-var': 'error',
+    'no-alert': 'error',
+    eqeqeq: 'error',
+    'prefer-const': 'warn',
+    'object-shorthand': 'warn'
+  }
+}
